e79594c32e4b9d453ef778dc0682820b699cbe57
Jakub <jakub.terkalski@gmail.com>
2022-12-19 11:33:49 +0100

    created project structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import styles from './App.module.scss';
+import Body from './components/Body/Body';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <Navbar />
+            <Body />
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
