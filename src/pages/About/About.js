1caf7b65dd59fb46ce7e0a172a6769433a811a64
Jakub <jakub.terkalski@gmail.com>
2022-12-23 12:49:46 +0100

    added about images and updated iframes size

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,23 +1,28 @@
 import SectionHeader from '../../components/SectionHeader';
 import styles from './About.module.scss';
 import AboutCard from './AboutCard';
-import image1 from '../../assets/images/3.jpg';
+import image1 from '../../assets/images/about/1.jpg';
+import image2 from '../../assets/images/about/2.jpg';
+import image3 from '../../assets/images/about/3.jpg';
+import image4 from '../../assets/images/about/4.jpg';
+import logo from '../../assets/images/about/FUNGY_LOGO.jpg';
 
 const About = () => {
     const text1 = `Jesteśmy młodym zespołem z Krakowa złożonym z czwórki dobrych przyjaciół.
-                Znamy się i tworzymy wspólnie od liceum. Do tej pory mamy nagrane cztery
-                single, z czego trzy z nich zostały już opublikowane. Ponadto graliśmy
+                Znamy się i tworzymy wspólnie od liceum.`;
+    const text2 = `Gramy własną muzykę w klimacie alternatywnego rock'a. Mega marzymy, żeby nasza muzyka leciała kiedyś w radio.`;
+    const text3 = `Do tej pory mamy nagrane cztery
+                single i pracujemy nad albumem składającym się z 6 utworów. Ponadto graliśmy
                 liczne koncerty w Krakowie.`;
+    const text4 = `Życie zespołowe to dla nas nie tylko opowiadanie nieśmiesznych żartów i dorywcze grywanie w niedziele o 17, to także wszelkie przedsięwzięcia audiowizualne które realizujemy wspólnie z naszymi przyjaciółmi.`;
+
     return (
         <div className={styles.about}>
             <SectionHeader text='O nas' />
             <AboutCard src={image1} text={text1} variant='right' />
-            <SectionHeader text='O nas' />
-            <AboutCard src={image1} text={text1} variant='left' />
-            <SectionHeader text='O nas' />
-            <AboutCard src={image1} text={text1} variant='right' />
-            <SectionHeader text='O nas' />
-            <AboutCard src={image1} text={text1} variant='left' />
+            <AboutCard src={image2} text={text2} variant='left' />
+            <AboutCard src={image3} text={text3} variant='right' />
+            <AboutCard src={image4} text={text4} variant='left' />
         </div>
     );
 };
